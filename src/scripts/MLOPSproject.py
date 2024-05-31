d9798f281ac4b8d69b0a233c0cb27a7bea1875ba
bmitra18 <144478628+bmitra18@users.noreply.github.com>
2024-05-30 20:15:37 -0500

    Updated debugging and info logging

--- src/scripts/MLOPSproject.py
+++ src/scripts/MLOPSproject.py
@@ -21,12 +21,13 @@ from rich.logging import RichHandler
 
 # Dataset import
 def importData():
+    log.debug("Data is being opened and processed")
     ccvi = pd.read_csv('data/Chicago_COVID-19_Community_Vulnerability_Index__CCVI__-_ZIP_Code_Only.csv')
     COVstats = pd.read_csv('data/COVID-19_Cases__Tests__and_Deaths_by_ZIP_Code.csv')
     COVvacc = pd.read_csv('data/COVID-19_Vaccinations_by_ZIP_Code_-_Historical.csv',low_memory=False)
     foodInsp = pd.read_csv('data/Food_Inspections_20240322.csv')
     pop = pd.read_csv('data/Chicago_Population_Counts.csv')
-    log.info("Data imported")
+    log.debug("Data imported")
     log.debug("CCVI len:"+str(len(ccvi)))
     log.debug("COVstats len:" + str(len(COVstats)))
     log.debug("COVvacc len:" + str(len(COVvacc)))
@@ -45,7 +46,7 @@ def cleanCOVIDStats(COVstats):
         'Deaths - Weekly': 'sum',
         'Case Rate - Weekly': lambda x: x.median()
     }).reset_index()
-    log.info("COVID-19 stats cleaned")
+    log.debug("COVID-19 stats cleaned")
     log.debug("covstats_cleaned len:" + str(len(covstats_cleaned)))
     return covstats_cleaned
 
@@ -58,14 +59,13 @@ def cleanCOVIDVacc(COVvacc):
     aggregated_data_dose = covdose_cleaned.groupby('Zip Code').agg({
         'Total Doses - Daily': 'sum',
     }).reset_index()
-    log.info("COVID-19 Vaccination cleaned")
+    log.debug("COVID-19 Vaccination cleaned")
     log.debug("COVIDvacc_cleaned len:" + str(len(aggregated_data_dose)))
     return aggregated_data_dose
 
 # CCVI Cleaning
 def cleanCCVI(ccvi):
     ccvi = ccvi[['Community Area or ZIP Code', 'CCVI Score', 'Location']]
-    log.info("Data imported")
     log.debug("covstats_cleaned len:" + str(len(ccvi)))
     return ccvi
 
@@ -74,41 +74,26 @@ def cleanFoodInspection(foodInsp):
     foodInsp.dropna(inplace=True)
     # Create a boolean mask to filter out entries with specific result values
     mask = foodInsp['Results'].isin(['Pass', 'Fail'])
-
     # Apply the mask to filter out rows with specified result values
     filtered_foodInsp = foodInsp[mask]
-
     # Count the entries by the 'Results' column
     result_counts = filtered_foodInsp['Results'].value_counts()
-
-    # Print the result counts
-    # log.debug(result_counts)
-
     filtered_foodInsp = filtered_foodInsp[['Zip', 'Results', 'Location']]
     filtered_foodInsp['Zip'] = filtered_foodInsp['Zip'].astype('int64')
-    #filtered_foodInsp
-
     # Group by 'Zip Code' and 'Results' columns and count the entries
     counts_by_zip_results = filtered_foodInsp.groupby(['Zip', 'Results']).size()
-
-    # Print the counts
-    # log.debug(counts_by_zip_results)
-
     # Rename 'Pass w/ Conditions' to 'Pass'
     food_inspections_grouped = counts_by_zip_results.copy()
     # log.debug(food_inspections_grouped)
-
     # Calculate pass-to-fail ratio for each ZIP code
     pass_fail_ratio = food_inspections_grouped.loc[:, 'Pass'] / food_inspections_grouped.loc[:, 'Fail']
     pass_fail_ratio = pass_fail_ratio.reset_index()
     pass_fail_ratio.columns = ['Zip', 'Results']
     pass_fail_ratio['Results'] = pass_fail_ratio['Results'].fillna(0)
-
-    # Print pass-to-fail ratio
-    # log.debug(pass_fail_ratio)
-    log.info("Food inspections grouped into pass/fail ratio by zip")
+    log.debug("Food inspections grouped into pass/fail ratio by zip")
     log.debug("pass_fail len:" + str(len(pass_fail_ratio)))
     return pass_fail_ratio
+
 # Population Cleaning
 def cleanPopulation(pop):
     # Create a boolean mask to filter out entries with specific result values
@@ -121,7 +106,7 @@ def cleanPopulation(pop):
     filtered_pop = filtered_pop[maskTwo].reset_index()
     pop_final = filtered_pop[['Geography', 'Population - Total']]
     pop_final.loc[:, 'Geography'] = pop_final['Geography'].astype('int64')
-    log.info("Population cleaned")
+    log.debug("Population cleaned")
     log.debug("pop_cleaned len:" + str(len(pop_final)))
     return pop_final
 
@@ -138,11 +123,8 @@ def mergeData(aggregated_data,aggregated_data_dose, pop_final,pass_fail_ratio,cc
     merged_data.rename(columns={'Deaths - Weekly': 'Total COVID Deaths'}, inplace=True)
     merged_data.rename(columns={'Total Doses - Daily': 'Total COVID Vacc Doses'}, inplace=True)
     merged_data.rename(columns={'Results': 'Food Insp: Pass/Fail'}, inplace=True)
-    log.info("Data Merged and filtered")
-    # print(merged_data)
-    # log.debug(merged_data.info())
-    # log.debug(merged_data.describe())
-    # log.debug("Merged list len:" + str(len(merged_data)))
+    log.debug("Data Merged and filtered")
+    log.debug("Merged list len:" + str(len(merged_data)))
     return merged_data
 
 # Split training data
@@ -152,9 +134,9 @@ def splitTrainingData(merged_data):
     X = merged_data.drop('Total COVID Deaths', axis=1)
     # Split the dataset into training and testing sets (80% train, 20% test)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    log.debug("Data split into train and test")
     log.debug("Training set size:"+ str(len(X_train)))
     log.debug("Testing set size:"+ str(len(X_test)))
-    log.info("Data split into train and test")
     return X_train, X_test, y_train, y_test
 
 # Models
@@ -162,8 +144,10 @@ def linearReg(X_train, X_test, y_train, y_test):
     # Initialize the model
     model_lr = LinearRegression()
     # Train the model
+    log.debug("Training model")
     model_lr.fit(X_train, y_train)
     # Predict on the test set
+    log.debug("Testing model")
     y_pred_lr = model_lr.predict(X_test)
     # Calculate evaluation metrics
     mae_lr = mean_absolute_error(y_test, y_pred_lr)
@@ -177,8 +161,10 @@ def randomForestRegression(X_train, X_test, y_train, y_test):
     # Initialize the model
     model_rf = RandomForestRegressor(n_estimators=100, random_state=42)
     # Train the model
+    log.debug("Training model")
     model_rf.fit(X_train, y_train)
     # Predict on the test set
+    log.debug("Testing model")
     y_pred_rf = model_rf.predict(X_test)
     # Calculate evaluation metrics
     mae_rf = mean_absolute_error(y_test, y_pred_rf)
@@ -191,13 +177,12 @@ def randomForestRegression(X_train, X_test, y_train, y_test):
 def gbr(X_train, X_test, y_train, y_test):
     # Initialize the model
     model_gb = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
-
     # Train the model
+    log.debug("Training model")
     model_gb.fit(X_train, y_train)
-
     # Predict on the test set
+    log.debug("Testing model")
     y_pred_gb = model_gb.predict(X_test)
-
     # Calculate evaluation metrics
     mae_gb = mean_absolute_error(y_test, y_pred_gb)
     mse_gb = mean_squared_error(y_test, y_pred_gb)
@@ -208,23 +193,19 @@ def gbr(X_train, X_test, y_train, y_test):
 def svr(X_train, X_test, y_train, y_test):
     # Initialize the model with StandardScaler
     model_svr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.1))
-
     # Train the model
+    log.debug("Training model")
     model_svr.fit(X_train, y_train)
-
     # Predict on the test set
+    log.debug("Testing model")
     y_pred_svr = model_svr.predict(X_test)
-
     # Calculate evaluation metrics
     mae_svr = mean_absolute_error(y_test, y_pred_svr)
     mse_svr = mean_squared_error(y_test, y_pred_svr)
     rmse_svr = np.sqrt(mse_svr)
     log.info("SVR Completed")
     logResults(mae_svr, mse_svr, rmse_svr)
-    # print("\nSupport Vector Regression (SVR) Evaluation:")
-    # print("Mean Absolute Error (MAE):", mae_svr)
-    # print("Mean Squared Error (MSE):", mse_svr)
-    # print("Root Mean Squared Error (RMSE):", rmse_svr)
+
 def logResults(mae,mse,rmse):
     log.info("Mean Absolute Error (MAE):" + str(mae)
              + ", Mean Squared Error (MSE):" + str(mse)
@@ -233,7 +214,7 @@ def logResults(mae,mse,rmse):
 if __name__ == '__main__':
     FORMAT = "%(message)s"
     logging.basicConfig(
-        level="DEBUG", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
     )
     log = logging.getLogger("rich")
     log.info("Program Running")
